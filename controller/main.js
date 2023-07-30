20d1a4f3f57fe83946a38d54d51d26e7963ef338
Tung2709 <vuthanhtungautomation@gmail.com>
2023-07-31 03:12:00 +0700

    update

--- controller/main.js
+++ controller/main.js
@@ -147,7 +147,7 @@ getAPI();
 getElement("#btnAdd").onclick = () => {
   const typePerson = getElementAll(".form-group");
   const student = new Student();
-  getElement(".form-group #id").value="HS"
+  // getElement(".form-group #id").value="HS"
   typePerson.forEach((e) => {
     e.classList.add("d-none");
   });
@@ -173,7 +173,7 @@ getElement(".typePerson").onchange = () => {
   });
 
   if (typeSelect === "student") {
-    getElement(".form-group #id").value="HS"
+    // getElement(".form-group #id").value="HS"
     typePerson.forEach((e) => {
       const name = e.getAttribute("name");
       for (let i in student) {
@@ -183,7 +183,7 @@ getElement(".typePerson").onchange = () => {
       }
     });
   } else if (typeSelect === "teacher") {
-    getElement(".form-group #id").value="T"
+    // getElement(".form-group #id").value="T"
     typePerson.forEach((e) => {
       const name = e.getAttribute("name");
       for (let i in teacher) {
@@ -193,7 +193,7 @@ getElement(".typePerson").onchange = () => {
       }
     });
   } else if (typeSelect === "customer") {
-    getElement(".form-group #id").value="C"
+    // getElement(".form-group #id").value="C"
     typePerson.forEach((e) => {
       const name = e.getAttribute("name");
       for (let i in customer) {
@@ -251,18 +251,25 @@ getElement("#btnThem").onclick = () => {
   let student = GetItem("student");
   let teacher = GetItem("teacher");
   let customer = GetItem("customer");
-  if (type === "student") {
-    student.push(data);
-    putAPI("student", student);
-  } else if (type === "teacher") {
-    teacher.push(data);
-    putAPI("teacher", teacher);
-  } else if (type === "customer") {
-    customer.push(data);
-    putAPI("customer", customer);
+
+  if(!valid){
+    if (type === "student") {
+      student.push(data);
+      putAPI("student", student);
+    } else if (type === "teacher") {
+      teacher.push(data);
+      putAPI("teacher", teacher);
+    } else if (type === "customer") {
+      customer.push(data);
+      putAPI("customer", customer);
+      
+    }
+    getElement("#btnDong").click();
+  }else if(valid){
+    alert("Nhập đầy đủ thông tin")
+    return
   }
-  getElement("#btnDong").click();
-};
+  };
 
 // Nhấn nút tính điểm trung bình cho đối tượng học sinh
 window.btnTinh = (id) => {
@@ -707,44 +714,210 @@ getElement("#sapXep").onchange = () => {
 // Validation Student
 
 // check mã
+
+let valid=true
+// if(valid){
+//   getElement("#btnThem").addEventListener("submit", function(evt) {
+//     evt.preventDefault();})
+// }
 getElement("#id").onblur=()=>{
-  const id = getElement("#id").value
-  if(!VALIDATION.checkID("#id","HS")){
-    getElement("#tbId").innerHTML="Mã học sinh phải có HS đứng đầu"
+  const type = getElement(".typePerson").value;
+  let key, ob;
+  if(type==="student"){
+    key="HS"
+    ob="học sinh"
+  }else  if(type==="teacher"){
+    key="T"
+    ob="giảng viên"
+  }else  if(type==="customer"){
+    key="C"
+    ob="khách hàng"
+  }
+
+  if(VALIDATION.checkEmty("#id")){
+    getElement("#tbId").innerHTML=`Mã ${ob} không được bỏ trống`}
+  else if(!VALIDATION.checkID("#id",key)){
+    getElement("#tbId").innerHTML=`Mã ${ob} phải có ${key} đứng đầu`
   }else if(!VALIDATION.checkLength("#id",4,8)){
-    getElement("#tbId").innerHTML="Mã học sinh phải có độ dài từ 4 đến 8"
+    getElement("#tbId").innerHTML=`Mã ${ob} phải có độ dài từ 4 đến 8`
   }else if(!VALIDATION.checkLetterNumber("#id")){
-    getElement("#tbId").innerHTML="Mã học sinh phải bao gồm chữ và số"
-    console.log(!VALIDATION.checkLetterNumber("#id"))
-  }else if(VALIDATION.checkEmty("#id")){
-    getElement("#tbId").innerHTML="Mã học sinh không được bỏ trống"
+    getElement("#tbId").innerHTML=`Mã ${ob} phải bao gồm chữ và số`
+  }else if(VALIDATION.checktrung("#id",type)){
+    getElement("#tbId").innerHTML=`Mã ${ob} bị trùng hãy nhập mã khác`
   }else{
     getElement("#tbId").innerHTML=""
+    valid&=false
+    return
   }
+  valid=true
+
 }
 
 // check name
 getElement("#name").onblur=()=>{
   const id = getElement("#name").value
   if(VALIDATION.checkEmty("#name")){
-    getElement("#tbName").innerHTML="Tên học sinh không được bỏ trống"
+    getElement("#tbName").innerHTML="Tên không được bỏ trống"
   }
   else if(!VALIDATION.checkLetter("#name")){
-    getElement("#tbName").innerHTML="Tên học sinh phải là chữ"
+    getElement("#tbName").innerHTML="Tên phải là chữ"
   }
   else{
     getElement("#tbName").innerHTML=""
+    valid&=false
+    return
   }
+  valid=true
   
 }
 
-//check adress
+//check adress và tên công ty
 getElement("#adress").onblur=()=>{
   if(VALIDATION.checkEmty("#adress")){
     getElement("#tbAdress").innerHTML="Địa chỉ không được bỏ trống"
-  }else if(!VALIDATION.checkLetter("#adress")){
-    getElement("#tbAdress").innerHTML="Địa chỉ phải có chữ"
+  }else if(!VALIDATION.checkLetterNumber("#adress")){
+    getElement("#tbAdress").innerHTML="Địa chỉ phải có số và chữ"
   }else{
     getElement("#tbAdress").innerHTML=""
+    valid&=false
+    return
+  }
+  valid=true
+}
+getElement("#nameCompany").onblur=()=>{
+  if(VALIDATION.checkEmty("#nameCompany")){
+    getElement("#tbNameCompany").innerHTML="Địa chỉ không được bỏ trống"
+  }else{
+    getElement("#tbNameCompany").innerHTML=""
   }
 }
+
+//check email
+getElement("#email").onblur=()=>{
+  if(VALIDATION.checkEmty("#email")){
+    getElement("#tbEmail").innerHTML="Email không được bỏ trống"
+  }else if(!VALIDATION.checkEmail("#email")){
+    getElement("#tbEmail").innerHTML="Email không đúng định dạng @gmail.com"
+  }else{
+    getElement("#tbEmail").innerHTML=""
+    valid&=false
+    return
+  }
+  valid=true
+}
+
+// check điểm toán lý hóa
+getElement("#toan").onblur=()=>{
+  if(VALIDATION.checkEmty("#toan")){
+    getElement("#tbToan").innerHTML="Điểm toán không được bỏ trống"
+  }else if(!VALIDATION.checkMark("#toan")){
+    getElement("#tbToan").innerHTML="Điểm toán phải là số  và nằm trong thang điểm 10"
+  }else{
+    getElement("#tbToan").innerHTML=""
+    valid&=false
+    return
+  }
+  valid=true
+}
+getElement("#hoa").onblur=()=>{
+  if(VALIDATION.checkEmty("#hoa")){
+    getElement("#tbHoa").innerHTML="Điểm hóa không được bỏ trống"
+  }else if(!VALIDATION.checkMark("#hoa")){
+    getElement("#tbHoa").innerHTML="Điểm hóa phải là số  và nằm trong thang điểm 10"
+  }else{
+    getElement("#tbHoa").innerHTML=""
+    valid&=false
+    return
+  }
+  valid=true
+}
+getElement("#ly").onblur=()=>{
+  if(VALIDATION.checkEmty("#ly")){
+    getElement("#tbLy").innerHTML="Điểm lý không được bỏ trống"
+  }else if(!VALIDATION.checkMark("#ly")){
+    getElement("#tbLy").innerHTML="Điểm lý phải là số  và nằm trong thang điểm 10"
+  }else{
+    getElement("#tbLy").innerHTML=""
+    valid&=false
+    return
+  }
+  valid=true
+}
+
+//check ngày làm việc, lương ngày, bill
+getElement("#date").onblur=()=>{
+  if(VALIDATION.checkEmty("#date")){
+    getElement("#tbDate").innerHTML="Ngày làm việc không được bỏ trống"
+  }else if(!VALIDATION.checkNumber("#date")){
+    getElement("#tbDate").innerHTML="Ngày làm việc phải là số"
+  }else{
+    getElement("#tbDate").innerHTML=""
+    valid&=false
+    return
+  }
+  valid=true
+}
+
+getElement("#sale").onblur=()=>{
+  if(VALIDATION.checkEmty("#sale")){
+    getElement("#tbSale").innerHTML="Lương ngày không được bỏ trống"
+  }else if(!VALIDATION.checkNumber("#sale")){
+    getElement("#tbSale").innerHTML="Lương ngày phải là số"
+  }else{
+    getElement("#tbSale").innerHTML=""
+    valid&=false
+    return
+  }
+  valid=true
+}
+
+getElement("#bill").onblur=()=>{
+  if(VALIDATION.checkEmty("#bill")){
+    getElement("#tbBill").innerHTML="Giá hóa đơn không được bỏ trống"
+  }else if(!VALIDATION.checkNumber("#bill")){
+    getElement("#tbBill").innerHTML="Giá hóa đơn phải là số"
+  }else{
+    getElement("#tbBill").innerHTML=""
+    valid&=false
+    return
+  }
+  valid=true
+}
+
+// check đánh giá
+getElement("#feedback").onblur=()=>{
+  if(VALIDATION.checkEmty("#feedback")){
+    getElement("#tbFeedback").innerHTML="Đánh giá không được bỏ trống"
+  }else if(!VALIDATION.checkLetter("#feedback")){
+    getElement("#tbFeedback").innerHTML="Đánh giá phải là số"
+  }else{
+    getElement("#tbFeedback").innerHTML=""
+    valid&=false
+    return
+  }
+  valid=true
+}
+
+getElement("#btnDong").onclick=()=>{
+ const tb= getElementAll(".form-group p")
+ const input= getElementAll(".form-group input,.form-group textarea")
+ tb.forEach(e=>{
+  e.innerHTML=""
+ })
+ input.forEach(e=>{
+  e.value=""
+ })
+}
+
+getElement(".typePerson").onchange=()=>{
+  console.log("true")
+  const tb= getElementAll(".form-group p")
+ const input= getElementAll(".form-group input,.form-group textarea")
+ tb.forEach(e=>{
+  e.innerHTML=""
+ })
+ input.forEach(e=>{
+  e.value=""
+ })
+}
+
--- validation/validation.js
+++ validation/validation.js
@@ -1,5 +1,6 @@
 const getElement = (selector) => document.querySelector(selector);
 const getElementAll = (selector) => document.querySelectorAll(selector);
+const GetItem = (key) => JSON.parse(localStorage.getItem(key));
 const VALIDATION={
 	checkID(id,letter){
 	const value = getElement(id).value
@@ -18,8 +19,8 @@ const VALIDATION={
 	return letters.test(value)
 	},
 	checkLetterNumber(id){
-	const letterNumber =  /^(?=.*[0-9])(?=.*[a-zA-Z])(?!.*[^ a-zA-Z0-9]).*$/;
-	// const letterNumber = /^[a-z0-9A-Z_ÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚĂĐĨŨƠàáâãèéêìíòóôõùúăđĩũơƯĂẠẢẤẦẨẪẬẮẰẲẴẶẸẺẼỀỀỂưăạảấầẩẫậắằẳẵặẹẻẽềềểỄỆỈỊỌỎỐỒỔỖỘỚỜỞỠỢỤỦỨỪễếệỉịọỏốồổỗộớờởỡợụủứừỬỮỰỲỴÝỶỸửữựỳỵỷỹ\s\W|_]+$/;
+	// const letterNumber =  /^(?=.*[0-9])(?=.*[a-zA-Z])(?!.*[^ a-zA-Z0-9]).*$/;
+	const letterNumber =  /^(?=.*[0-9])(?=.*[a-zA-ZÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚĂĐĨŨƠàáâãèéêìíòóôõùúăđĩũơƯĂẠẢẤẦẨẪẬẮẰẲẴẶẸẺẼỀỀỂẾưăạảấầẩẫậắằẳẵặẹẻẽềềểếỄỆỈỊỌỎỐỒỔỖỘỚỜỞỠỢỤỦỨỪễệỉịọỏốồổỗộớờởỡợụủứừỬỮỰỲỴÝỶỸửữựỳỵỷỹ\s\W|_])(?!.*[^ a-zA-Z0-9ÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚĂĐĨŨƠàáâãèéêìíòóôõùúăđĩũơƯĂẠẢẤẦẨẪẬẮẰẲẴẶẸẺẼỀỀỂẾưăạảấầẩẫậắằẳẵặẹẻẽềềểếỄỆỈỊỌỎỐỒỔỖỘỚỜỞỠỢỤỦỨỪễệỉịọỏốồổỗộớờởỡợụủứừỬỮỰỲỴÝỶỸửữựỳỵỷỹ\s\W|_]).*$/;
 	return letterNumber.test(getElement(id).value)
 	},
 	checkLength(id,min,max){
@@ -30,6 +31,18 @@ const VALIDATION={
 		}
 		return true
 	},
+	checkLetterOrNumber(id){
+		const letterNumber =  /^(?=.*[0-9])(?=.*[a-zA-Z])(?!.*[^ a-zA-Z0-9]).*$/;
+		return letterNumber.test(getElement(id).value)
+		},
+		checkLength(id,min,max){
+			const value = getElement(id).value
+			const e=min
+			if(value.length<min||value.length>max){
+				return false
+			}
+			return true
+		},
 	checkEmail(id){
 		var email=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 		const value = getElement(id).value
@@ -39,9 +52,40 @@ const VALIDATION={
 		const value = getElement(id).value
 		return /^-?\d+$/.test(value);
 	},
+	checkMark(id) {
+		const value = getElement(id).value
+		return /^-?\d+$/.test(value)&&value>=0&&value<=10;
+	},
 	checkEmty(id){
 		const value = getElement(id).value
 		return !value.trim().length;
+	},
+	checktrung(id,type){
+	const value = getElement(id).value
+	const student=GetItem("student")
+	const teacher=GetItem("teacher")
+	const customer=GetItem("customer")
+	let check=false
+	if(type==="student"){
+		student.forEach(e => {
+			if(e.id===value){
+				check=true
+			}
+		});
+	}else if(type==="teacher"){
+		teacher.forEach(e => {
+			if(e.id===value){
+				check=true
+			}
+		});
+	}else if(type==="customer"){
+		customer.forEach(e => {
+			if(e.id===value){
+				check=true
+			}
+		});
+	}
+	return check
 	}
 }
 export {VALIDATION}
